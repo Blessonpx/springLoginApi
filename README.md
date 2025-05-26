c4ed52ba6c7f25b3e66b3321d949569a7fbbc312
Blessonpx <blessonxavierofficial@gmail.com>
2025-05-26 09:53:45 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Combining following 2 blogs
+
+https://medium.com/javarevisited/a-simple-user-authentication-api-made-with-spring-boot-4a7135ff1eca
+
+https://medium.com/javascript-in-plain-english/creating-a-simple-vue-js-website-for-our-backend-1d1ef8839c27
+
---
+++ src/main/java/springLoginApi/springLoginApi/SpringLoginApiApplication.java
@@ -0,0 +1,13 @@
+package springLoginApi.springLoginApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLoginApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLoginApiApplication.class, args);
+	}
+
+}
